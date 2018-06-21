bad20c17bbfb5f31c465db1df43407c1c7fdc404
Vince Angeloni <vangeloni@activecampaign.com>
2018-06-21 09:07:58 -0400

    Added visual feedback for color swatch copying

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,8 +16,41 @@
 //= require_tree .
 
 
-// Swatch Library
+// Global
+var animationEnd = (function(el) {
+  var animations = {
+    animation: 'animationend',
+    OAnimation: 'oAnimationEnd',
+    MozAnimation: 'mozAnimationEnd',
+    WebkitAnimation: 'webkitAnimationEnd',
+  };
 
+  for (var t in animations) {
+    if (el.style[t] !== undefined) {
+      return animations[t];
+    }
+  }
+})(document.createElement('div'));
 
-// Colors page
+// Toasts
+function showToast(message) {
+	var template = '<div class="toast">' + message + '</div>';
+
+	if (!$('.toast').length) { // Check if a toast is already visible first
+
+		// Add that shit to the DOM son!
+		$('body').prepend(template);
+		$('.toast').addClass('animated bounceInDown');
+		$('.toast').one(animationEnd, function() {
+			//$('.toast').removeClass('bounceInDown');
+			setTimeout(function() {
+				$('.toast').addClass('bounceOutUp');
+				$('.toast').one(animationEnd, function() {
+					$('.toast').remove();
+				});
+			}, 3000);
+
+		});
+	}
+}
 
