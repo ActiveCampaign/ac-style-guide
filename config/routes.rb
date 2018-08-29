f547481c29702889a0e55e84a178e391b35a4179
Vince Angeloni <vangeloni@activecampaign.com>
2018-08-29 09:05:58 -0400

    Created first pattern page for buttons

--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,10 @@ Rails.application.routes.draw do
   get '/resources/glossary' => 'resources#glossary'
   get '/resources/library' => 'resources#library'
 
+  # Patterns
+  get '/patterns' => 'patterns#index'
+  get '/patterns/buttons' => 'patterns#buttons'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
